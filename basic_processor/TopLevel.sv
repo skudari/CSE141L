1f55b912817bc152ba6efc82472ea3b57bb97e65
skudari <skudari@ucsd.edu>
2019-12-02 10:29:55 -0800

    Updated Top Level

--- basic_processor/TopLevel.sv
+++ basic_processor/TopLevel.sv
@@ -5,7 +5,7 @@
 // partial only										   
 module TopLevel(		   // you will have the same 3 ports
     input     start,	   // init/reset, active high
-	input     CLK,		   // clock -- posedge used inside design
+    input     CLK,		   // clock -- posedge used inside design
     output    halt		   // done flag from DUT
     );
 
@@ -16,86 +16,65 @@ wire [ 7:0] InA, InB, 	   // ALU operand inputs
             ALU_out;       // ALU result
 wire [ 7:0] regWriteValue, // data in to reg file
             memWriteValue, // data in to data_memory
-	   	    Mem_Out;	   // data out from data_memory
+	    Mem_Out;	   // data out from data_memory
 wire        MEM_READ,	   // data_memory read enable
-		    MEM_WRITE,	   // data_memory write enable
-			reg_wr_en,	   // reg_file write enable
-			sc_clr,        // carry reg clear
-			sc_en,	       // carry reg enable
-		    SC_OUT,	       // to carry register
-			ZERO,		   // ALU output = 0 flag
-			BEVEN,		   // ALU input B is even flag
+	    MEM_WRITE,	   // data_memory write enable
+	    reg_wr_en,	   // reg_file write enable
+	    sc_clr,        // carry reg clear
+	    sc_en,	       // carry reg enable
+	    SC_OUT,	       // to carry register
+	    ZERO,		   // ALU output = 0 flag
+	    BEVEN,		   // ALU input B is even flag
             jump_en,	   // to program counter: jump enable
             branch_en;	   // to program counter: branch enable
 logic[15:0] cycle_ct;	   // standalone; NOT PC!
 logic       SC_IN;         // carry register (loop with ALU)
+	
+	
+	
+	const logic [2:0]load  = 3'b100;
+	const logic [2:0]store  = 3'b101;	
+	
+	
+	logic [7:0] regA; 
+	logic [7:0] regB; 
+	wire [7:0] MSW; 
+	wire [7:0] LSW; 
 
-// Fetch = Program Counter + Instruction ROM
-// Program Counter
-  PC PC1 (
-	.init       (start), 
-	.halt              ,  // SystemVerilg shorthand for .halt(halt), 
-	.jump_en           ,  // jump enable
-	.branch_en	       ,  // branch enable
-	.CLK        (CLK)  ,  // (CLK) is required in Verilog, optional in SystemVerilog
-	.PC             	  // program count = index to instruction memory
-	);					  
-
-// Control decoder
-  Ctrl Ctrl1 (
-	.Instruction,    // from instr_ROM
-	.ZERO,			 // from ALU: result = 0
-	.BEVEN,			 // from ALU: input B is even (LSB=0)
-	.jump_en,		 // to PC
-	.branch_en		 // to PC
-  );
-// instruction ROM
-  InstROM instr_ROM1(
-	.InstAddress   (PC), 
-	.InstOut       (Instruction)
+	program1 callProg1(
+		.regA(data_outA),
+		.regB(data_outB), 
+		.MSW(MSW), 
+		.LSW(LSW)
 	);
+	//if instruction is store: pass in MSW and LSW into regFile 	
+	//if instruction is load: pass in DataOutA and DataOutB into regFile
+	
+	//load from data mem into register
+	reg_file load(		
+		.CLK(CLK),
+		.write_en(1'b1),
+		.raddrA({1'b0, 3'b100}),
+		.raddrB({1'b0, 3'b101}),
+		.waddr({1'b0, 3'b100}),
+		.data_inA(DataOutA),
+		.data_inB(DataOutB),
+		.data_outA(regA),
+		.data_outB(regB)
+		);
 
-  assign load_inst = Instruction[8:6]==3'b110;  // calls out load specially
-
-// reg file
-	reg_file #(.W(8),.D(4)) reg_file1 (
-		.CLK    				  ,
-		.write_en  (reg_wr_en)    , 
-		.raddrA    ({1'b0,Instruction[5:3]}),         //concatenate with 0 to give us 4 bits
-		.raddrB    ({1'b0,Instruction[2:0]}), 
-		.waddr     ({1'b0,Instruction[5:3]+1}), 	  // mux above
-		.data_in   (regWriteValue) , 
-		.data_outA (ReadA        ) , 
-		.data_outB (ReadB		 )
-	);
-// one pointer, two adjacent read accesses: (optional approach)
-//	.raddrA ({Instruction[5:3],1'b0});
-//	.raddrB ({Instruction[5:3],1'b1});
-
-    assign InA = ReadA;						          // connect RF out to ALU in
-	assign InB = ReadB;
-	assign MEM_WRITE = (Instruction == 9'h111);       // mem_store command
-	assign regWriteValue = load_inst? Mem_Out : ALU_out;  // 2:1 switch into reg_file
-    ALU ALU1  (
-	  .INPUTA  (InA),
-	  .INPUTB  (InB), 
-	  .OP      (Instruction[8:6]),
-	  .OUT     (ALU_out),//regWriteValue),
-	  .SC_IN   ,//(SC_IN),
-	  .SC_OUT  ,
-	  .ZERO ,
-	  .BEVEN
-	  );
-  
 	data_mem data_mem1(
-		.DataAddress  (ReadA)    , 
+		.CLK(CLK),
+		.reset (start), 
+		.DataAddressIn  (newDataAddress)    , //HOW TO DERTERMINE ADDRESS INCREMENT?
 		.ReadMem      (1'b1),          //(MEM_READ) ,   always enabled 
 		.WriteMem     (MEM_WRITE), 
-		.DataIn       (memWriteValue), 
-		.DataOut      (Mem_Out)  , 
-		.CLK 		  		     ,
-		.reset		  (start)
-	);
+		.DataInA       (), 
+		.DataInB       (),
+		.DataOutA      (DataOutA), //A is mem[1] 
+		.DataOutB      (DataOutB), 	//B is mem[0] 
+	);	
+	
 	
 // count number of instructions executed
 always_ff @(posedge CLK)
