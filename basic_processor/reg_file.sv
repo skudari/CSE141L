824cc4145fba9d25ebe17655166856cc7cdd0d4a
skudari <skudari@ucsd.edu>
2019-12-02 10:30:16 -0800

    reg file reads two addresses

--- basic_processor/reg_file.sv
+++ basic_processor/reg_file.sv
@@ -7,25 +7,30 @@
 module reg_file #(parameter W=8, D=3)(		 // W = data path width; D = pointer width
   input           CLK,
                   write_en,
+                  start,
   input  [ D-1:0] raddrA,
                   raddrB,
-                  waddr,
-  input  [ W-1:0] data_in,
+                  waddrA,
+  input  [ W-1:0] memAddress,
+  input  [ W-1:0] data_inA,
+  input  [ W-1:0] data_inB,
+  
   output [ W-1:0] data_outA,
   output logic [W-1:0] data_outB
-    );
-
+  );
+  
 // W bits wide [W-1:0] and 2**4 registers deep 	 
 logic [W-1:0] registers[2**D];	  // or just registers[16] if we know D=4 always
 
 // combinational reads w/ blanking of address 0
-assign      data_outA = raddrA? registers[raddrA] : '0;	 // can't read from addr 0, just like MIPS
+always_comb data_outA = registers[raddrA];   
 always_comb data_outB = registers[raddrB];               // can read from addr 0, just like ARM
 
 // sequential (clocked) writes 
 always_ff @ (posedge CLK)
   if (write_en && waddr)	                             // && waddr requires nonzero pointer address
 // if (write_en) if want to be able to write to address 0, as well
-    registers[waddr] <= data_in;
+    registers[waddr] <= data_inA;
+    registers[waddr + 3'b001] <= data_inB;
 
 endmodule
