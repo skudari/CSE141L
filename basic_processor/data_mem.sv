4b5be13dd41168fa6815951dbc6063f6b44c86f7
skudari <skudari@ucsd.edu>
2019-12-02 10:27:31 -0800

    Data Mem takes two bytes in consecutive addresses

--- basic_processor/data_mem.sv
+++ basic_processor/data_mem.sv
@@ -9,21 +9,27 @@ module data_mem(
   input [7:0]        DataAddress,
   input              ReadMem,
   input              WriteMem,
-  input [7:0]        DataIn,
-  output logic[7:0]  DataOut);
+  input [7:0]        DataInA, //from registers
+  input [7:0]       DataInB,
+  output logic[7:0]  DataOutA, 
+  output logic[7:0] DataOutB,
+);
 
   logic [7:0] core[256];
-
-//  initial 
+  logic [7:0] writeDataAddress; 
+   
 //    $readmemh("dataram_init.list", my_memory);
   always_comb                     // reads are combinational
     if(ReadMem) begin
-      DataOut = core[DataAddress];
+      DataOutB = core[DataAddress]; 
+      DataOutA = corew[DataAddress + 8'b00000001];
 // optional diagnostic print
-	  $display("Memory read M[%d] = %d",DataAddress,DataOut);
-    end else 
-      DataOut = 'bZ;           // tristate, undriven
-
+	    $display("Memory read M[%d] = %d ",DataAddress,DataOutB);
+	    $display("Memory read M[%d] = %d ",DataAddress + 8'b00000001,DataOutA);
+    end else begin 
+      DataOutA = 'bZ;           // tristate, undriven
+      DataOutB = 'bZ;
+    end
   always_ff @ (posedge CLK)		 // writes are sequential
     if(reset) begin
 // you may initialize your memory w/ constants, if you wish
@@ -33,9 +39,11 @@ module data_mem(
       core[244] <= 5;
 	end
     else if(WriteMem) begin
-      core[DataAddress] <= DataIn;
+	    assign writeDataAddress = DataAddress + 8'b00011110;
+	    core[writeDataAddress] <= DataIn;
 // optional diagnostic print statement
 	  $display("Memory write M[%d] = %d",DataAddress,DataIn);
     end
 
+    assign newDataAddress = DataAddress + 8'b00000010; 
 endmodule
